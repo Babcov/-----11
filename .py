da74b47186b14551595b839cc49552af04219ca8
Alexander <babkow_av.jur@mail.ru>
2023-08-17 15:41:43 +0300

    12

--- .py
+++ .py
@@ -1,8 +1,8 @@
-n = int(input())
- 
+number = int(input('Введите число: '))
 factorial = 1
-while n > 1:
-    factorial *= n
-    n -= 1
- 
+
+while number > 1:
+    factorial = factorial * number
+    number = number - 1
+
 print(factorial)
